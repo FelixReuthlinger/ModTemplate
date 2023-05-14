4bbad2cde00399f06736e172242345d2f877fba3
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-05-14 15:54:46 +0200

    make a simple template

---
+++ ModTemplatePlugin.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ModTemplate;
+
+[BepInPlugin(ModGuid, ModName, ModVersion)]
+public class ModTemplatePlugin : BaseUnityPlugin
+{
+    private const string ModName = "ModTemplate";
+    private const string ModVersion = "1.0.0";
+    private const string Author = "{your name here}";
+    private const string ModGuid = Author + "." + ModName;
+    private const string ConfigFileName = ModGuid + ".cfg";
+    private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+    private readonly Harmony _harmony = new(ModGuid);
+
+    public static readonly ManualLogSource ModTemplateLogger =
+        BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {ModGuid} is loaded!");
+        
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        _harmony.PatchAll(assembly);
+    }
+}
